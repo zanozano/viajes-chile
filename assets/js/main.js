668df45cd71a95e1a5872b5fa75891d6597a75f2
Cristobal Andres Manzano Martinez <zano@live.cl>
2021-06-28 20:35:44 -0400

    Tercera actualizacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
     $(".nav-link").click(function (event) {
-      event.preventDefault();
-  
-      var smoothScroll = this.hash;
-  
-      $("html").animate({
-          scrollTop: $(smoothScroll).offset().top - 56,
-        },
-        800,
-      );
+        event.preventDefault();
+
+        var smoothScroll = this.hash;
+
+        $("html").animate({
+                scrollTop: $(smoothScroll).offset().top - 56,
+            },
+            800,
+        );
     });
-  });
-  $('[data-toggle="tooltip"]').tooltip()
+});
+$('[data-toggle="tooltip"]').tooltip()
