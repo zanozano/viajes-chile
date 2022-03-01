dcda59dc6a67c80038441c6cdab5cdad705e946b
Cristobal Andres Manzano Martinez <zano@live.cl>
2022-02-28 23:33:16 -0300

    Modificacion de codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,44 @@
 /* Scroll to top */
-$(".go-to-top").click(function (event) {
-    $("html").animate({
-            scrollTop: 0,
-        },
-        800,
-    );
+$('.go-to-top').click(function (event) {
+	$('html').animate(
+		{
+			scrollTop: 0,
+		},
+		800
+	);
 });
 /* Smooth Scroll */
 $(document).ready(function () {
-    $(".link-smooth").click(function (event) {
-        event.preventDefault();
+	$('.link-smooth').click(function (event) {
+		event.preventDefault();
 
-        var smoothScroll = this.hash;
+		var smoothScroll = this.hash;
 
-        $("html").animate({
-                scrollTop: $(smoothScroll).offset().top - 65,
-            },
-            800,
-        );
-    });
+		$('html').animate(
+			{
+				scrollTop: $(smoothScroll).offset().top - 65,
+			},
+			800
+		);
+	});
 });
 /* Scroll Top */
 /* Tooltip */
-$('[data-toggle="tooltip"]').tooltip()
+$('[data-toggle="tooltip"]').tooltip();
 
 /* Carousel */
 $('.carousel').carousel({
-    interval: 3000
-})
+	interval: 3000,
+});
 /* Background Topbar inverse */
 $(window).scroll(function () {
-    if ($("#navbar").offset().top > 66) {
-        $("#navbar").addClass("bg-new");
-    } else {
-        $("#navbar").removeClass("bg-new");
-    }
+	if ($('#navbar').offset().top > 66) {
+		$('#navbar').addClass('bg-new');
+	} else {
+		$('#navbar').removeClass('bg-new');
+	}
 });
 /* Toggle Content */
 $('.card-title').on('click', function () {
-    $('.card-text').toggle();
+	$('.card-text').toggle();
 });
